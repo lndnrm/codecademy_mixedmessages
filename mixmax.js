cff939b7cf1db3572bbc91855dfeb253e5c0f380
mlle <lndnrm@gmail.com>
2021-02-04 18:19:12 +0100

    second commit - project outline

---
+++ mixmax.js
@@ -0,0 +1,12 @@
+/* 
+Whenever this programm runs, a random message should be presented to the user, together with his username
+*/
+
+// define messages we want to put out randomly
+const message1 =[[mes1], [mes2], [mes3]]
+
+// define username
+
+// define randomizer
+
+// define output
