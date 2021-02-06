b5827946c81aacf48cd933865e88f71ee0caba57
mlle <lndnrm@gmail.com>
2021-02-06 22:29:18 +0100

    third commit - first draft of programming routing - very rocky, just ideas, needs fixes

--- mixmax.js
+++ mixmax.js
@@ -1,12 +1,17 @@
 /* 
-Whenever this programm runs, a random message should be presented to the user, together with his username
+Whenever this programm runs, a random message should be presented to the user.
 */
 
 // define messages we want to put out randomly
-const message1 =[[mes1], [mes2], [mes3]]
+const message =[[mes1], [mes2], [mes3]]
 
 // define username
+
+// define weekday
+let date = getdate(today);
+let user = getusername(username);
 
 // define randomizer
 
 // define output
+console.log('Hello ${user}! Today is ${date}. /n ${message} ')
